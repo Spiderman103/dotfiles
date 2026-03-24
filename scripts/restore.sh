dd001d2344dc40c84c80db2531bc6b7d67b156b5
Spiderman103 <sahil.narsinghani14@gmail.com>
2026-03-24 13:48:04 -0400

    Revised Installation Script

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 # restore.sh
-# Run this on a fresh Arch install to restore everything
+# Run this on a fresh Arch install to restore your setup
 
-echo "Installing paru (AUR helper)..."
+set -e  # stop on any error
+
+DOTFILES=~/spDotfiles
+
+echo "==> Installing paru (AUR helper)..."
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/paru.git /tmp/paru
 cd /tmp/paru && makepkg -si
 cd ~
 
-echo "Installing packages..."
-sudo pacman -S --needed - < ~/dotfiles/pkglist-explicit.txt
+echo "==> Installing explicit packages..."
+sudo pacman -S --needed - < "$DOTFILES/pkglist-explicit.txt"
 
-echo "Installing AUR packages..."
-paru -S --needed - < ~/dotfiles/pkglist-aur.txt
+echo "==> Installing AUR packages..."
+paru -S --needed - < "$DOTFILES/pkglist-aur.txt"
 
-echo "Restoring configs..."
+echo "==> Restoring configs..."
 mkdir -p ~/.config ~/.local/bin
-cp -r ~/dotfiles/config/* ~/.config/
-cp -r ~/dotfiles/local/bin/* ~/.local/bin/
+cp -r "$DOTFILES/config/"* ~/.config/
+cp -r "$DOTFILES/local/"* ~/.local/
+
+echo "==> Setting permissions on local bin..."
 chmod +x ~/.local/bin/*
 
-echo "Restoring shell config..."
-cp ~/dotfiles/.bashrc ~/
-# cp ~/dotfiles/.zshrc ~/   # uncomment if using zsh
+echo "==> Restoring shell config..."
+cp "$DOTFILES/.bashrc" ~/
+# cp "$DOTFILES/.zshrc" ~/  # uncomment if using zsh
 
-echo "Done. Log out and back in."
+echo "==> Done. Log out and back in to apply changes."
